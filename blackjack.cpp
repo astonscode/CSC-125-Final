1be3cd3272e9f65cc2140088db12b048ecf54575
andmatt321 <andmatt@umich.edu>
2025-10-27 15:57:42 -0400

    Initial Setup: 3

--- blackjack.cpp
+++ blackjack.cpp
@@ -9,45 +9,47 @@ using namespace std;
 int main()
 {
 
-    int gameLoop = 0;
+    bool continueGame = true;
 
-    while (gameLoop != -1)
+    while (continueGame)
     {
+        //create display menu function for here
+        int choice;
+        cout<<"Welcome to Blackjack!\n"
+          "Please choose from the options below"<<endl;
+        cout<<"1. Play Game\n"
+              "2. Directions\n"
+              "3. Hi-Scores\n"
+              "4. Exit\n";
 
-        switch(gameLoop)
+        cin>>choice;
+
+        switch(choice)
         {
         case 0:
-            //create display menu function for here
-            cout<<"Welcome to Blackjack!\n"
-              "Please choose from the options below"<<endl;
-            cout<<"1. Play Game\n"
-                  "2. Directions\n"
-                  "3. Hi-Scores\n"
-                  "4. Exit\n";
-            cin>>gameLoop;
+
             break;
         case 1:
             //game logic
             cout<<"Playing game...\n"
                   "...Now we're done.\n\n";
-            gameLoop = 0;
+            //Insert Gameplay function
             break;
         case 2:
             //Directions
             cout<<"This is how you play the game!\n\n";
-            gameLoop = 0;
+            //Insert Directions display function
             break;
         case 3:
             //show hi-scores
             cout<<"Hi Scores!\n\n";
-            gameLoop = 0;
+            //Insert Hi-scores display function
             break;
         case 4:
-            gameLoop = -1;
+            continueGame = false;
             break;
         default:
             cout<<"Invalid choice.\n";
-            gameLoop = 0;
             break;
 
 
